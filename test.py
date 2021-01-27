48aa5b1703e4a400cb7f6cd34058bba8fc002960
Chad Furman <chad@chadfurman.com>
2021-01-27 08:58:41 -0500

    wip: test.py mapping out plans for receipt scanner

---
+++ test.py
@@ -0,0 +1,9 @@
+// rotates the images
+// sends the combined images to Google OCR
+// parses out the results for product name and price
+// de-duplicates all product names
+// checks product names against known name DB and fills in known names
+// checks product names against known category db and fills in category
+// turns product names, categories, and prices into a CSV
+// sends CSV to google sheets
+
