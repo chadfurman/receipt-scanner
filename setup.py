c04e59aaa919a55c77ae373909738a65a7a296a4
Chad Furman <chad@chadfurman.com>
2021-01-28 08:42:17 -0500

    wip: setup.py added but doesn't work yet

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='receipt-scanner',
+      version='0.1',
+      description='Takes pictures of receipts, orients them, sends them to Google\'s OCR API, parses the output into names and prices, adds categories, creates a CSV, and sends the CSV to Sheets',
+      url='http://github.com/chadfurman/receipt-scanner',
+      author='Chad Furman',
+      author_email='chad@chadfurman.com',
+      license='MIT',
+      packages=['receipt-scanner'],
+      install_requires=[
+          'pillow',
+          'google-cloud-vision'
+      ],
+      zip_safe=False)
